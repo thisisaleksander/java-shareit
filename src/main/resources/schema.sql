5a0719af9aa80a23d5923e43decbc902c1c01201
alexander <alexandersizov01@gmail.com>
2023-06-29 23:39:20 +0300

    update schema.sql and pom.xml

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,37 +2,39 @@ create table if not exists users (
     id bigint generated always as identity not null primary key,
     email varchar(50) not null,
     name varchar(250) not null,
-    unique (email)
+    unique (email, id)
 );
 
 create table if not exists items (
     id bigint generated always as identity not null primary key,
-    user_id bigint not null,
-    description varchar(200),
-    count bigint,
-    available varchar(50),
-    name varchar(100),
-    constraint fk_items_to_users foreign key (user_id) references users(id),
+    name varchar(50),
+    description varchar(250),
+    is_available varchar(50),
+    owner_id bigint not null,
+    request_id bigint,
+    constraint fk_items_to_users foreign key (owner_id) references users(id),
     unique (id)
 );
 
 create table if not exists comments (
     id bigint generated always as identity not null primary key,
     item_id bigint,
-    author_name varchar(250),
+    author_id bigint,
     created timestamp,
-    text varchar(1000),
-    constraint items foreign key (item_id) references items(id)
+    text varchar(250),
+    constraint fk_comments_to_items foreign key (item_id) references items(id),
+    constraint fk_comments_to_users foreign key (author_id) references users(id),
+    unique (id)
 );
 
 create table if not exists bookings (
     id bigint generated always as identity not null primary key,
-    user_id bigint not null,
+    booker_id bigint not null,
     item_id bigint not null,
-    status varchar(200),
-    start timestamp,
-    finish timestamp,
-    constraint fk_booking_to_users foreign key (user_id) references users(id),
+    status varchar(50),
+    start_time timestamp,
+    end_time timestamp,
+    constraint fk_booking_to_users foreign key (booker_id) references users(id),
     constraint fk_booking_to_items foreign key (item_id) references items(id),
-    UNIQUE (id)
+    unique (id)
 );
