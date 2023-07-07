037697d13c864845ec9017093077e0d83d9ad80d
alexander <alexandersizov01@gmail.com>
2023-07-07 15:34:41 +0300

    update resources

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -13,9 +13,10 @@ create table if not exists items (
     count bigint,
     available varchar(50),
     name varchar(100),
+    request_id  BIGINT,
     constraint pk_items primary key (id),
     constraint fk_items_to_users foreign key (user_id) references users(id),
-    constraint uq_owner_item_name unique (user_id, name)
+    unique (id)
 );
 
 create table if not exists comments (
@@ -37,5 +38,16 @@ create table if not exists bookings (
     finish timestamp,
     constraint pk_bookings primary key (id),
     constraint fk_booking_to_users foreign key (user_id) references users(id),
-    constraint fk_booking_to_items foreign key (item_id) references items(id)
+    constraint fk_booking_to_items foreign key (item_id) references items(id),
+    unique (id)
 );
+
+create table if not exists item_request (
+    id bigint generated always as identity not null,
+    user_id bigint,
+    description varchar(1000),
+    created timestamp,
+    constraint pk_item_request primary key (id),
+    unique (id)
+);
+
